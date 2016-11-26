2e2ddf974915179b81336bb3419a7cc564e1cada
etovladislav <etovladislav@yandex.ru>
2016-11-26 11:22:35 +0300

    model

---
+++ src/main/java/ru/hackathon/vk/model/Book.java
@@ -0,0 +1,10 @@
+package ru.hackathon.vk.model;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by etovladislav on 26.11.16.
+ */
+@Entity
+public class Book {
+}
---
+++ src/main/java/ru/hackathon/vk/model/User.java
@@ -0,0 +1,12 @@
+package ru.hackathon.vk.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by etovladislav on 26.11.16.
+ */
+@Entity
+@Table(name = "users")
+public class User {
+}
